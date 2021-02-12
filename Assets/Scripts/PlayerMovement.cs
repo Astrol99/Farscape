50b8e979fc5ccb4b3d3283afe9d85511976c806e
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-12 01:12:26 -0600

    Actual Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+        else if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+        jump = false;
+    }
+}
