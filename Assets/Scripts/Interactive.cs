2043a00f06984da8579414147fe82c9e96a03679
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-15 03:13:08 -0600

    Added interact function

--- Assets/Scripts/Interactive.cs
+++ Assets/Scripts/Interactive.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactive : MonoBehaviour
 {
@@ -20,9 +21,27 @@ public class Interactive : MonoBehaviour
         }
     }
 
+    private GameObject createText(string text, float delay)
+    {
+        GameObject textObj = new GameObject("interactText");
+        textObj.transform.SetParent(this.transform);
+
+        Text textComponent = textObj.AddComponent<Text>();
+        textComponent.text = text;
+
+        return textObj;
+    }
+
     private void Update()
     {
         if (collided)
+        {
             sprite.color = Color.Lerp(Color.white, new Color(255, 255, 0, 0.5f), Mathf.PingPong(Time.time, 1));
+            
+            if (Input.GetButtonDown("Interact"))
+            {
+                createText("I AM A TURTLE", 10f);
+            }
+        }
     }
 }
