25619f0028b4a6c1f7a118f9cd77f122b9c4c45b
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-15 01:50:33 -0600

    Added interactions with collision layers

---
+++ Assets/Scripts/Interactive.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Interactive : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    private bool collided = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            collided = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            sprite.color = Color.white;
+            collided = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (collided)
+            sprite.color = Color.Lerp(Color.white, new Color(255, 255, 0, 0.5f), Mathf.PingPong(Time.time, 1));
+    }
+}
