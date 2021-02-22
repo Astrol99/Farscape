8fd7a23255ac2399fb242087420e2a251f555b9d
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-22 05:21:56 -0600

    Added idle movement script to be applied to all interactive entities and swapped trigger and collider of interactive entities

---
+++ Assets/Scripts/Idle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Idle : MonoBehaviour
+{
+    public float speed = 1f;
+    public float delay = 2f;
+
+    private bool move = false;
+    private Vector2 direction = Vector2.right;
+
+    private void Start()
+    {
+        InvokeRepeating("toggleMovement", 0, delay);
+        InvokeRepeating("toggleDirection", 0, delay*2);
+    }
+
+    void toggleMovement() 
+    {
+        move = !move;
+    }
+
+    void toggleDirection()
+    {
+        direction *= -1;
+        transform.localScale = new Vector2(direction.x, 1);
+    }
+
+    private void Update()
+    {
+        if (move)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+    }
+}
