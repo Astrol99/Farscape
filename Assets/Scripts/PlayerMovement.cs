9101f207ddbbc1eba5accaab400da35ba7468ae5
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-22 11:24:00 -0600

    Added a bunch of levels and background images

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         speed = defaultSpeed;
+        animator.SetBool("IsJumping", true);
     }
 
     private void Update()
