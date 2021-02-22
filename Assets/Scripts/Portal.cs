d510849e701a98f977adc75b444b05c72fc6d9db
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-22 12:05:37 -0600

    buncha stuff

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    public bool StartPortal = false;
+    private float timeToFade = 1.0f;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartPortal)
+        {
+            renderer.material.color = Color.Lerp(renderer.material.color, Color.clear, timeToFade * Time.deltaTime);
+        }
+    }
+}
